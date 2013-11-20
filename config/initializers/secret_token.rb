ea1057376a1f7f74a16361684d3a5675b00b0434
Sunil Rao <sunil2rao@gmail.com>
2013-11-19 23:03:20 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Petals::Application.config.secret_key_base = 'b6489f3f789447e2197fbe9caebb3929e0033468d2484261078876b7bf18c2b710f93721a876e9fa4cc3c0a61e9c7ed858c95c9a427f44589eec53472c6d8696'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Petals::Application.config.session_store :cookie_store, key: '_petals_session'
